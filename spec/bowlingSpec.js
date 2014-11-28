d6fece5c4aea956fc6e03249868fbba7a4a80db6
Alan Bridger <alanrbridger@gmail.com>
2014-11-28 14:50:46 +0000

    The game contains ten frames, with two scores for each of the first nine, and 3 scores for the tenth.

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -9,12 +9,29 @@ describe('Player', function() {
 
 describe('Game', function(){
 
-  describe('starting the game', function() {
+  describe('initialising the game', function() {
 
-    it('has a name', function() {
+    beforeEach(function(){
       player = new Player("Angus McTavish");
       game = new Game(player);
+    });
+
+    it('has a player', function() {
       expect(game.player).toEqual(player);
+    });
+
+    it('has ten frames', function(){
+      expect(Object.keys(game.FRAMES).length).toEqual(10);
+    });
+
+    it('the first nine frames must have two scores', function() {
+      for(var key in game.FRAMES) {
+        if (key !== 'round10') expect(game.FRAMES[key].length).toEqual(2);
+      };
+    });
+
+    it('the tenth frame must have three scores', function() {
+      expect(game.FRAMES['round10'].length).toEqual(3);
     });
 
   });
--- src/bowling.js
+++ src/bowling.js
@@ -5,3 +5,16 @@ function Player(name) {
 function Game(player) {
   this.player = player;
 };
+
+Game.prototype.FRAMES = {
+  'round1'  : [0,0],
+  'round2'  : [0,0],
+  'round3'  : [0,0],
+  'round4'  : [0,0],
+  'round5'  : [0,0],
+  'round6'  : [0,0],
+  'round7'  : [0,0],
+  'round8'  : [0,0],
+  'round9'  : [0,0],
+  'round10' : [0,0,0]
+};
