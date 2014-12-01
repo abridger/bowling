8cb67c5bbc541ea2f30ed38e192fbac024bd1677
Alan Bridger <alanrbridger@gmail.com>
2014-12-01 09:29:44 +0000

    Keeps track of total of each frame - needs to take account of strikes and half-strikes.

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -27,24 +27,46 @@ describe('Scoresheet', function(){
   });
 
   describe('using the scoresheet', function() {
+
     beforeEach(function() {
-      frame = 'frame1';
-      points = 10;
+      for(var i=1; i < 4; i++) {
+        scoresheet.addPoints('frame1', 10);
+        scoresheet.addPoints('frame10', 10);
+      }
     });
 
     describe('it should be possible to', function() {
       it('add scores to a frame', function() {
-        scoresheet.addPoints(frame, points);
-        expect(scoresheet.FRAMES['frame1']).toContain(points)
+        expect(scoresheet.FRAMES['frame1']['scores']).toContain(10)
+      });
+      it('add three scores to the final frame', function() {
+        expect(scoresheet.FRAMES['frame10']['scores'].length).toEqual(3);
       });
     });
 
     describe('it should not be possible to', function() {
+      it('add a score higher than ten', function() {
+
+      });
       it('add more than two scores to one of the first nine frames', function() {
-        for(var i=1; i < 3; i++) {
-          scoresheet.addPoints(frame, points);
-        }
-        expect(scoresheet.FRAMES['frame1'].length).toEqual(2);
+        expect(scoresheet.FRAMES['frame1']['scores'].length).toEqual(2);
+      });
+      it('add more than three scores to the final frame', function() {
+        expect(scoresheet.FRAMES['frame10']['scores'].length).toEqual(3);
+      });
+    });
+
+  });
+
+  describe('scoring', function() {
+
+    describe('it should calculate', function() {
+      it('the cumulative score for each frame', function() {
+        scoresheet.addPoints('frame1', 8);
+        scoresheet.addPoints('frame1', 8);
+        scoresheet.addPoints('frame2', 8);
+        scoresheet.sumPoints();
+        expect(scoresheet.FRAMES['frame2']['total']).toBe(24);
       });
     });
 
--- src/bowling.js
+++ src/bowling.js
@@ -7,18 +7,45 @@ function Scoresheet(player) {
 };
 
 Scoresheet.prototype.FRAMES = {
-  'frame1'  : [],
-  'frame2'  : [],
-  'frame3'  : [],
-  'frame4'  : [],
-  'frame5'  : [],
-  'frame6'  : [],
-  'frame7'  : [],
-  'frame8'  : [],
-  'frame9'  : [],
-  'frame10' : []
+  'frame1'  : {'total' : null, 'scores' : []},
+  'frame2'  : {'total' : null, 'scores' : []},
+  'frame3'  : {'total' : null, 'scores' : [8]},
+  'frame4'  : {'total' : null, 'scores' : [8]},
+  'frame5'  : {'total' : null, 'scores' : [8]},
+  'frame6'  : {'total' : null, 'scores' : [8]},
+  'frame7'  : {'total' : null, 'scores' : [8]},
+  'frame8'  : {'total' : null, 'scores' : [8]},
+  'frame9'  : {'total' : null, 'scores' : [8]},
+  'frame10' : {'total' : null, 'scores' : [8  ]}
 };
 
 Scoresheet.prototype.addPoints= function(frame, points){
-  if(this.FRAMES[frame].length < 2) this.FRAMES[frame].push(points);
+  if(frame === 'frame10') {
+    if(this.FRAMES[frame]['scores'].length < 3)
+      this.FRAMES[frame]['scores'].push(points);
+  } else {
+    if(this.FRAMES[frame]['scores'].length < 2)
+      this.FRAMES[frame]['scores'].push(points);
+  }
+};
+
+Scoresheet.prototype.sumPoints = function(){
+  var accumulator = 0;
+
+  for (var frame in this.FRAMES) {
+
+    var targetFrame = this.FRAMES[frame]
+
+    var sum = targetFrame['scores'].reduce(function(previousValue, currentValue){
+      return previousValue + currentValue;
+    });
+
+    targetFrame['total'] = sum;
+    accumulator = accumulator + sum;
+
+    console.log(targetFrame);
+    console.log(accumulator);
+
+    // Need to include bonuses for strikes and half strikes.
+  }
 };
